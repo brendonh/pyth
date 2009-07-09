4164d206094df4c2dc96936ad811962e0f60cec0
Brendon Hogger <brendonh@gmail.com>
2009-07-10 05:20:14 +0800

    Most of RTF1.5

--- examples/plaintext.py
+++ examples/plaintext.py
@@ -1,35 +1,6 @@
-
-from pyth.plugins.python.reader import *
 from pyth.plugins.plaintext.writer import PlaintextWriter
+import pythonDoc
 
-doc = PythonReader.read((
-    P [
-      T(ITALIC, url=u'http://www.google.com') [ u"Hello World, " ],
-      u"hee hee hee! ", u"This seems to work"
-    ],
-    L [
-      [unicode(word) for word in ("One", "Two", "Three", "Four")]
-    ],
-    L [
-      u"Introduction",
-      LE [
-        u"First sentence in the\nsub-section",
-        u"Also some other stuff",
-        L [
-         u"Alpha",
-         L [
-           u"Beta\nWhomble",
-           LE [ u"Beta", u"Whoop\nWhoa" ],
-           u"Beta",
-         ],
-         u"Gamma",
-         u"Gamma",
-        ],
-        u"Final sentence in the sub-section",
-      ],
-      u"Conclusion",
-    ],
-    u"That's all, folks!"
-))
+doc = pythonDoc.buildDoc()
 
 print PlaintextWriter.write(doc).getvalue()

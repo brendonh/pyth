a1b9e75ac52955fcc3f91aa26aec9e9dab1c22ba
Brendon Hogger <brendonh@gmail.com>
2009-11-23 18:17:29 +0800

    More on packaging

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup, find_packages
 setup(name="pyth",
       version="0.5",
       packages = find_packages(),
+      zip_safe = False,
 
       description="Python text markup conversion",
       author="Brendon Hogger",
       author_email="brendonh@taizilla.com",
       url="http://wiki.github.com/brendonh/pyth",
+      long_description=open('README').read(),
 )

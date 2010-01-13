de9f0708a473c5eaabba66c6e4f0077f139eb246
Brendon Hogger <brendonh@gmail.com>
2010-01-13 20:29:43 +0800

    Detect non-RTF files early, and handle symbol font

--- pyth/plugins/rtf15/reader.py
+++ pyth/plugins/rtf15/reader.py
@@ -11,25 +11,46 @@ import string, re, itertools
 from pyth import document
 from pyth.format import PythReader
 
-
 _CONTROLCHARS = set(string.ascii_letters + string.digits + "-*")
 _DIGITS = set(string.digits)
+
+# Maps Symbol typeface to Unicode, extracted from http://en.wikipedia.org/wiki/Symbol_(typeface)
+symbolTable = {
+    33: 33, 34: 8704, 35: 35, 36: 8707, 37: 37, 38: 38, 39: 8717, 40: 40, 41: 41, 42: 42, 43: 43, 44: 44, 45: 45, 46: 46, 
+    47: 47, 48: 48, 49: 49, 50: 50, 51: 51, 52: 52, 53: 53, 54: 54, 55: 55, 56: 56, 57: 57, 58: 58, 59: 59, 60: 60, 61: 61,
+    62: 62, 63: 63, 64: 8773, 65: 913, 66: 914, 67: 935, 68: 916, 69: 917, 70: 934, 71: 915, 72: 919, 73: 921, 74: 977, 
+    75: 922, 76: 923, 77: 924, 78: 925, 79: 927, 80: 928, 81: 920, 82: 929, 83: 931, 84: 932, 85: 933, 86: 962, 87: 937, 
+    88: 926, 89: 936, 90: 918, 91: 91, 92: 8756, 93: 93, 94: 8869, 95: 95, 96: 63717, 97: 945, 98: 946, 99: 967, 100: 948, 
+    101: 949, 102: 966, 103: 947, 104: 951, 105: 953, 106: 981, 107: 954, 108: 955, 109: 956, 110: 957, 111: 959, 112: 960, 
+    113: 952, 114: 961, 115: 963, 116: 964, 117: 965, 118: 982, 119: 969, 120: 958, 121: 968, 122: 950, 123: 123, 124: 124, 
+    125: 125, 126: 126, 160: 8364, 161: 978, 162: 697, 163: 8804, 164: 8260, 165: 8734, 166: 402, 167: 9827, 168: 9830, 
+    169: 9829, 170: 9824, 171: 8596, 172: 8592, 173: 8593, 174: 8594, 175: 8595, 176: 176, 177: 177, 178: 698, 179: 8805, 
+    180: 215, 181: 8733, 182: 8706, 183: 8226, 184: 247, 185: 8800, 186: 8801, 187: 8776, 188: 8230, 189: 9168, 190: 9135, 
+    191: 8629, 192: 8501, 193: 8465, 194: 8476, 195: 8472, 196: 8855, 197: 8853, 198: 8709, 199: 8745, 200: 8746, 201: 8835, 
+    202: 8839, 203: 8836, 204: 8834, 205: 8838, 206: 8712, 207: 8713, 208: 8736, 209: 8711, 210: 174, 211: 169, 212: 8482, 
+    213: 8719, 214: 8730, 215: 8901, 216: 172, 217: 8743, 218: 8744, 219: 8660, 220: 8656, 221: 8657, 222: 8658, 223: 8659,
+    224: 9674, 225: 12296, 226: 174, 227: 169, 228: 8482, 229: 8721, 230: 9115, 231: 9116, 232: 9117, 233: 9121, 234: 9122, 
+    235: 9123, 236: 9127, 237: 9128, 238: 9129, 239: 9130, 241: 12297, 242: 8747, 243: 8992, 244: 9134, 245: 8993, 246: 9118, 
+    247: 9119, 248: 9120, 249: 9124, 250: 9125, 251: 9126, 252: 9131, 253: 9132, 254: 9133}
 
 _CODEPAGES = {
     0: "cp1252",   # ANSI
     1: "cp1252",   # Default (this is wrong, but there is no right)
 
-    # Does Python have built-in support for these? What is it?
-    # 2: "42",     # Symbol
+    2: symbolTable,   # Symbol
     77: "mac-roman", # Mac Roman
+
+    # Does Python have built-in support for these? What is it?
     # 78: "10001", # Mac Shift Jis
     # 79: "10003", # Mac Hangul
     # 80: "10008", # Mac GB2312
     # 81: "10002", # Mac Big5
     # 83: "10005", # Mac Hebrew
+
     84: "mac-arabic", # Mac Arabic
     85: "mac-greek", # Mac Greek
     86: "mac-turkish", # Mac Turkish
+
     # 87: "10021", # Mac Thai
     # 88: "10029", # Mac East Europe
     # 89: "10007", # Mac Russian
@@ -51,6 +72,9 @@ _CODEPAGES = {
     254: "cp437",  # PC 437
     255: "cp850",  # OEM
 }
+
+
+
 
 
 class BackslashEscape(Exception):
@@ -76,6 +100,13 @@ class Rtf15Reader(PythReader):
 
     def go(self):
         self.source.seek(0)
+
+        if self.source.read(5) != r"{\rtf":
+            from pyth.errors import WrongFileType
+            raise WrongFileType("Doesn't look like an RTF file")
+
+        self.source.seek(0)
+
         self.group = Group()
         self.charsetTable = None
         self.stack = [self.group]
@@ -408,10 +439,23 @@ class Group(object):
 
 
     def handle_ansi_escape(self, code):
-        try:
-            self.content.append(chr(int(code, 16)).decode(self.charset))
-        except UnicodeDecodeError:
-            self.content.append('?')
+        code = int(code, 16)
+
+        if isinstance(self.charset, dict):
+            uni_code = self.charset.get(code)
+            if uni_code is None:
+                char = '?'
+            else:
+                char = unichr(uni_code)
+            
+
+        else:
+            try:
+                char = chr(code).decode(self.charset)
+            except UnicodeDecodeError:
+                char = '?'
+
+        self.content.append(char)
 
 
     def handle_control_symbol(self, symbol):
